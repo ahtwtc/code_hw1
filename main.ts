f115dc73aa45f560dc943d837790afbd02d039f0
ahtwtc <71689237+ahtwtc@users.noreply.github.com>
2020-10-19 17:24:20 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,32 @@
+basic.forever(function () {
+    for (let x = 0; x <= 4; x++) {
+        if (x == 1) {
+            led.plot(4, x)
+            basic.pause(200)
+            led.plot(3, x)
+            basic.pause(200)
+            led.plot(2, x)
+            basic.pause(200)
+            led.plot(1, x)
+            basic.pause(200)
+            led.plot(0, x)
+            basic.pause(200)
+        } else if (x == 3) {
+            led.plot(4, x)
+            basic.pause(200)
+            led.plot(3, x)
+            basic.pause(200)
+            led.plot(2, x)
+            basic.pause(200)
+            led.plot(1, x)
+            basic.pause(200)
+            led.plot(0, x)
+            basic.pause(200)
+        } else {
+            for (let y = 0; y <= 4; y++) {
+                led.plot(y, x)
+                basic.pause(200)
+            }
+        }
+    }
+})
